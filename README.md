9f8222c25fef4d3e5a05ea41632430bde1bcf232
Crolette <73672696+crolette@users.noreply.github.com>
2021-08-02 17:08:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Frontend Mentor - Rock, Paper, Scissors solution
+
+This is a solution to the [Rock, Paper, Scissors challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Play Rock, Paper, Scissors against the computer
+- **Bonus**: Play Rock, Paper, Scissors, Lizard, Spock against the computer
+
+### Screenshot
+
+![](../images/screen1.png)
+![](../images/screen2.png)
+
+
+### Links
+
+- Solution URL: (https://your-solution-url.com)
+- Live Site URL: (https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Desktop-first workflow
+- JS Vanilla
+
+
+### Continued development
+
+Continue to improve my JS and CSS grid skills.
+
+## Author
+
+- Website - [Crolweb](https://www.crolweb.be)
+- Frontend Mentor - [crolette](https://www.frontendmentor.io/profile/crolette)
+- Codepen - [crolette](https://codepen.io/crolette/pens/)
+- LinkedIn - [Jonathan](https://www.linkedin.com/in/jonathan-de-dijcker-6a7b1532/)
---
+++ app.js
@@ -0,0 +1,108 @@
+let scoreUser = 0;
+const score = document.querySelector("#score");
+const gameChoices = ["rock", "paper", "scissors"];
+let userChoice = "";
+let houseChoice = "";
+const choices = [
+  ...document.querySelector("[data-step='1']").querySelectorAll("[data-choice"),
+];
+const step1 = document.querySelector("[data-step='1']");
+const step2 = document.querySelector("[data-step='2']");
+const result = document.querySelector(".result");
+const playAgain = document.querySelector(".play");
+const house = document.querySelector("[data-choice='house']");
+const user = document.querySelector("[data-choice='user']");
+
+const rulesBtn = document.querySelector(".btn__rules");
+const rules = document.querySelector(".rules");
+const closeBtn = document.querySelector(".btn__close");
+
+rulesBtn.addEventListener("click", () => {
+  rules.classList.remove("hide");
+});
+
+closeBtn.addEventListener("click", () => {
+  rules.classList.add("hide");
+});
+
+choices.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    userChoice = e.target.dataset.choice;
+    goToStep2();
+  });
+});
+
+function goToStep2() {
+  step1.classList.add("hide");
+  step2.classList.remove("hide");
+  user.classList.remove("win");
+  house.classList.remove("win");
+
+  const houseChoice = gameChoices[Math.floor(Math.random() * 3)];
+  setUserChoice();
+  randomHouse();
+}
+
+function setUserChoice() {
+  user.children[0].innerHTML = `<img src="./images/icon-${userChoice}.svg" alt="" />`;
+  user.setAttribute("data-choice", userChoice);
+}
+
+function setHouseChoice(houseChoice) {
+  house.children[0].innerHTML = `<img src="./images/icon-${houseChoice}.svg" alt="" />`;
+  house.setAttribute("data-choice", houseChoice);
+}
+
+function randomHouse() {
+  let index = 0;
+  let intervalID = setInterval(() => {
+    if (index === 3) {
+      index = 0;
+    }
+    houseChoice = gameChoices[index];
+    setHouseChoice(houseChoice);
+    index++;
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(intervalID);
+
+    whoWins(gameChoices[Math.floor(Math.random() * 3)]);
+  }, 1000);
+}
+
+function whoWins(houseChoice) {
+  setHouseChoice(houseChoice);
+  let win = false;
+
+  if (userChoice === "paper" && houseChoice == "rock") {
+    win = true;
+  } else if (userChoice === "rock" && houseChoice == "scissors") {
+    win = true;
+  } else if (userChoice === "scissors" && houseChoice == "paper") {
+    win = true;
+  }
+
+  setTimeout(() => setScore(win), 200);
+}
+
+function resetGame() {
+  step1.classList.remove("hide");
+  step2.classList.add("hide");
+  result.classList.add("hide");
+}
+
+function setScore(win) {
+  result.classList.remove("hide");
+  if (win === true) {
+    document.querySelector("#result-text").textContent = "YOU WIN";
+    scoreUser++;
+    score.textContent = scoreUser;
+    user.classList.add("win");
+  } else {
+    document.querySelector("#result-text").textContent = "YOU LOSE";
+    house.classList.add("win");
+  }
+
+  result.addEventListener("click", () => resetGame());
+}
